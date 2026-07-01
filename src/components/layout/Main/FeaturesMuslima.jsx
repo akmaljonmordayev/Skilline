ad7da8a3e284346ff70b42fd52dc08bdccf26ca7
akmaljonmordayev <akmaljonmordayev@gmail.com>
2026-07-01 18:22:10 +0500

    project structure edited and completed

---
+++ src/components/layout/Main/FeaturesMuslima.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FeaturesMuslima() {
+  return (
+    <div>FeaturesMuslima</div>
+  )
+}
+
+export default FeaturesMuslima
---
+++ src/components/layout/Main/ManagementDiyor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ManagementDiyor() {
+  return (
+    <div>ManagementDiyor</div>
+  )
+}
+
+export default ManagementDiyor
