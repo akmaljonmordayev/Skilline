15f65b193fe974788bc6c75cf6a96a5f36fa0e05
ozodbeksunattilo <ozodbeksunattilo@gmail.com>
2026-07-03 17:15:33 +0500

    Section add

--- src/components/layout/Main/Section2Otabek.jsx
+++ src/components/layout/Main/Section2Otabek.jsx
@@ -19,54 +19,58 @@ function Section2Otabek() {
         margin: "0 auto",
       }}
     >
-    <div style={{ textAlign: "center" }}>
-  <h1>Trusted by 5,000+ Companies Worldwide</h1>
-</div>
+      <div style={{ textAlign: "center" }}>
+        <h1>Trusted by 5,000+ Companies Worldwide</h1>
+      </div>
       <div
-    style={{
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginTop: "60px",
-    width: "100%",
-  }}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginTop: "60px",
+          width: "100%",
+        }}
       >
         <a target="_blank" href="https://www.google.com/?hl=ru">
-          <img style={{height:"30px",width:"auto"}} src={Gogole} alt="" />
+          <img style={{ height: "30px", width: "auto" }} src={Gogole} alt="" />
         </a>
         <a target="_blank" href="https://www.netflix.com/uz-ru/">
-          <img style={{height:"30px",width:"auto"}} src={Netflix} alt="" />
+          <img style={{ height: "30px", width: "auto" }} src={Netflix} alt="" />
         </a>
         <a target="_blank" href="https://www.airbnb.ru/">
-          <img style={{height:"30px",width:"auto"}} src={Airbnb} alt="" />
+          <img style={{ height: "30px", width: "auto" }} src={Airbnb} alt="" />
         </a>
         <a target="_blank" href="https://www.amazon.com/">
-          <img style={{height:"30px",width:"auto"}} src={Amazon} alt="" />
+          <img style={{ height: "30px", width: "auto" }} src={Amazon} alt="" />
         </a>
         <a target="_blank" href="https://www.facebook.com/?locale=ru_RU">
-          <img style={{height:"30px",width:"auto"}} src={Facebook} alt="" />
+          <img
+            style={{ height: "30px", width: "auto" }}
+            src={Facebook}
+            alt=""
+          />
         </a>
         <a target="_blank" href="https://www.grab.com/sg/">
-          <img style={{height:"30px",width:"auto"}} src={Grab} alt="" />
+          <img style={{ height: "30px", width: "auto" }} src={Grab} alt="" />
         </a>
       </div>
       <div>
         <h1
           style={{
-    color: "#2F327D",
-    textAlign: "center",
-    marginTop: "100px",
-  }}
+            color: "#2F327D",
+            textAlign: "center",
+            marginTop: "100px",
+          }}
         >
-          What is <span style={{ color: "#F3AC50"  }}>Skilline?</span>
+          What is <span style={{ color: "#F3AC50" }}>Skilline?</span>
         </h1>
         <p
-       style={{
-  fontSize: "16px",
-  width: "700px",
-  margin: "20px auto 0",
-  textAlign: "center",
-}}
+          style={{
+            fontSize: "16px",
+            width: "700px",
+            margin: "20px auto 0",
+            textAlign: "center",
+          }}
         >
           Skilline is a platform that allows educators to create online classes
           whereby they can store the course materials online; manage
@@ -75,13 +79,13 @@ function Section2Otabek() {
         </p>
       </div>
       <div
-  style={{
-  display: "flex",
-  justifyContent: "space-between",
-  gap: "30px",
-  marginTop: "100px",
-  marginBottom: "200px",
-}}
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          gap: "30px",
+          marginTop: "100px",
+          marginBottom: "200px",
+        }}
       >
         <div
           style={{
@@ -141,12 +145,14 @@ function Section2Otabek() {
                 border: "2px solid white",
               }}
             >
-             Enter access code
+              Enter access code
             </button>
           </div>
         </div>
       </div>
+
     </div>
+    
   );
 }
 
